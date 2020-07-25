b67905ed540b56c0251b73a4e006b21834408000
Trammell hudson <hudson@trmm.net>
2020-07-25 19:51:26 +0200

    hello: demonstrate 16-bit arithmetic and pwm

--- hello.c
+++ hello.c
@@ -14,17 +14,29 @@ void uart_puts(const char * s)
 		uart_putc(*s++);
 }
 
+static void __attribute__((__noinline__)) pwm(uint8_t b, uint8_t led)
+{
+	for(uint8_t i = 0 ; i < b ; i++)
+		PORTB = led;
+
+	for(uint8_t i = b ; i < 255 ; i++)
+		PORTB = 0;
+}
+
+
 int main(void)
 {
-	uint8_t cycle = 0;
+	uint16_t cycle = 0;
 	static const char hexdigit[16] = "0123456789abcdef";
 
 	while(1)
 	{
-		PORTB = 1;
-		uart_putc(hexdigit[(cycle >> 4) & 0xF]);
-		uart_putc(hexdigit[(cycle >> 0) & 0xF]);
-		PORTB = 0;
+		uart_putc(hexdigit[(cycle >> 12) & 0xF]);
+		uart_putc(hexdigit[(cycle >>  8) & 0xF]);
+		uart_putc(hexdigit[(cycle >>  4) & 0xF]);
+		uart_putc(hexdigit[(cycle >>  0) & 0xF]);
+		pwm(cycle >> 4, 1);
+
 		uart_puts(" Hello world\r\n");
 		cycle++;
 	}

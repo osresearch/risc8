b4fa39f93615ed801df38d02f1057e2874a48701
Trammell hudson <hudson@trmm.net>
2020-07-25 17:12:17 +0200

    blink demo: pwm the LED

--- blink.c
+++ blink.c
@@ -1,21 +1,36 @@
 #include <avr/io.h>
 #include <stdint.h>
 
+static void __attribute__((__noinline__)) pwm(uint8_t b, uint8_t led)
+{
+	for(uint8_t i = 0 ; i < b ; i++)
+		PORTB = led;
+
+	for(uint8_t i = b ; i < 255 ; i++)
+		PORTB = 0;
+}
+
 int main(void)
 {
-	uint8_t cycle = 0;
-
+	uint8_t led = 1;
 	while(1)
 	{
-		PORTB = cycle;
-		cycle = (cycle + 1) & 7;
-
-		for(unsigned i = 0 ; i < 60000  ; i++)
+		for(uint8_t b = 1 ; b < 64; b++)
 		{
-			asm("nop\n nop\n nop\n nop");
-			asm("nop\n nop\n nop\n nop");
-			asm("nop\n nop\n nop\n nop");
-			asm("nop\n nop\n nop\n nop");
+			for(uint8_t s = 0 ; s < 64 ; s++)
+				pwm(b, led);
 		}
+
+		for(uint8_t b = 64 ; b > 0 ; b--)
+		{
+			for(uint8_t s = 0 ; s < 64 ; s++)
+				pwm(b, led);
+		}
+
+		if (led == 4)
+			led = 1;
+		else
+			led <<= 1;
+		PINB = led;
 	}
 }

01246fecca1ed4fc5565abb173e782715fd16b94
Trammell hudson <hudson@trmm.net>
2020-07-28 19:27:20 +0200

    hello: cycle up and down, delay uart writes to avoid glitching

--- hello.c
+++ hello.c
@@ -7,6 +7,8 @@ void uart_putc(char c)
 {
 	while((USISR & 1) == 0)
 		;
+	for(uint16_t i = 0 ; i < 100 ; i++)
+		asm("nop");
 	USIDR = c;
 }
 
@@ -25,7 +27,7 @@ static void __attribute__((__noinline__)) pwm(uint8_t b, uint8_t led)
 		PORTB = 0;
 }
 
-static char hexdigit(uint8_t x)
+static char __attribute__((__noinline__)) hexdigit(uint8_t x)
 {
 	static const char PROGMEM hexdigit[16] = "0123456789abcdef";
 	return pgm_read_byte_near(hexdigit + (x & 0xF));
@@ -33,18 +35,29 @@ static char hexdigit(uint8_t x)
 
 int main(void)
 {
-	uint16_t cycle = 1 << 12;
+	uint16_t cycle = 0xFADE;
+
+	*(volatile uint8_t*) 0x20 = hexdigit(cycle >> 4);
+	*(volatile uint8_t*) 0x20 = hexdigit(cycle >> 0);
 
 	while(1)
 	{
-		pwm(cycle >> 4, cycle >> 12);
+		uint8_t bright = cycle >> 4;
+		if (bright & 0x80)
+			bright = ~bright;
+		pwm(bright, cycle >> 12);
 
+		cycle++;
+		if ((cycle & 0xFFF) != 0)
+			continue;
+
+		uart_putc(hexdigit(bright >> 4));
+		uart_putc(hexdigit(bright >> 0));
+		uart_putc(' ');
 		uart_putc(hexdigit(cycle >> 12));
 		uart_putc(hexdigit(cycle >>  8));
 		uart_putc(hexdigit(cycle >>  4));
 		uart_putc(hexdigit(cycle >>  0));
 		uart_puts(" Hello world\r\n");
-
-		cycle++;
 	}
 }
